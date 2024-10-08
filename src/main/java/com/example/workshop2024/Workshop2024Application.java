0e77fdc4d62fed4393d956b3d13c22af11dcf2a1
lucie <lucie.delannoy@ecoles-epsi.net>
2024-10-08 09:58:03 +0200

    first commit

---
+++ src/main/java/com/example/workshop2024/Workshop2024Application.java
@@ -0,0 +1,13 @@
+package com.example.workshop2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workshop2024/Workshop2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshop2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
